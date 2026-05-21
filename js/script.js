56841b3140eba893a541a4971d9ee51d4349e5f9
Thomasskola <thomas.talia@elev.ga.ntig.se>
2026-05-21 12:49:00 +0200

    evelinstener update

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ console.log("det fungerar")
 
 document.addEventListener("keydown", (event) => {
 
-console.log("update in 3 2 1 aaaand delayed by lebron")
+console.log(event.key);
 
-})
+if(event.key = "t"){
+document.getElementById("a").style = "color:red";
+
+}})
