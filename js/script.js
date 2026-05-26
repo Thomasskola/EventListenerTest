d1d532bd9c874b16029eb8c8ce7c038a544116b6
Thomasskola <thomas.talia@elev.ga.ntig.se>
2026-05-26 11:32:03 +0200

    test

--- js/script.js
+++ js/script.js
@@ -5,7 +5,12 @@ document.addEventListener("keydown", (event) => {
 
 console.log(event.key);
 
-if(event.key == "t"){
+const sound = new Audio("graaa.mp3")
+
+if(event.key == "m") {
 document.getElementById("a").style = "color:red";
+sound.currentTime = 0;
+
+sound.play();
 
 }})
